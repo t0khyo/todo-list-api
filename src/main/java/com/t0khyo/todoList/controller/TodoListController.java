3062ef4ff19e8f5d4cc773fb8dec11b364756407
t0khyo <abdullrhman.eltoukhy@gmail.com>
2023-09-17 01:43:46 +0300

    TodoListController implemented

--- src/main/java/com/t0khyo/todoList/controller/TodoListController.java
+++ src/main/java/com/t0khyo/todoList/controller/TodoListController.java
@@ -1,5 +1,6 @@
 package com.t0khyo.todoList.controller;
 
+import com.t0khyo.todoList.dto.TodoListDTO;
 import com.t0khyo.todoList.entity.TodoList;
 import com.t0khyo.todoList.service.TodoListService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,43 +22,47 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/todo-lists")
 public class TodoListController {
-    private final TodoListService todoService;
+    private final TodoListService todoListService;
 
     @Autowired
-    public TodoListController(TodoListService todoService) {
-        this.todoService = todoService;
+    public TodoListController(TodoListService todoListService) {
+        this.todoListService = todoListService;
     }
 
     // Get all todoLists
     @GetMapping("/")
     public ResponseEntity<List<TodoList>> getAllTodoLists() {
-        return null;
+        return new ResponseEntity<>(todoListService.findAll(), HttpStatus.FOUND);
     }
 
     // Get a todoList by its ID
     @GetMapping("/{todoListId}")
     public ResponseEntity<TodoList> getTodoListById(@PathVariable("todoListId") Long todoListId) {
-        return null;
+        TodoList todoList = todoListService.findById(todoListId);
+        return new ResponseEntity<>(todoList, HttpStatus.FOUND);
     }
 
     // Create a new todoList
     @PostMapping("/")
-    public ResponseEntity<TodoList> createTodoList(@RequestBody TodoList todoList) {
-        return null;
+    public ResponseEntity<TodoList> createTodoList(@RequestBody TodoListDTO todoListDTO) {
+        TodoList newTodList = todoListService.save(todoListDTO);
+        return new ResponseEntity<>(newTodList, HttpStatus.CREATED);
     }
 
     // Update a todoList by its ID
     @PutMapping("/{todoListId}")
     public ResponseEntity<TodoList> updateTodoList(
             @PathVariable("todoListId") Long todoListId,
-            @RequestBody TodoList providedTodo
+            @RequestBody TodoListDTO todoListDTO
     ) {
-        return null;
+        TodoList updatedTodoList = todoListService.update(todoListId, todoListDTO);
+        return ResponseEntity.ok(updatedTodoList);
     }
 
     // Delete a todoList by its ID
     @DeleteMapping("/{todoListId}")
     public ResponseEntity<String> deleteTodoList(@PathVariable("todoListId") long todoListId) {
-        return null;
+        todoListService.deleteById(todoListId);
+        return ResponseEntity.noContent().build();
     }
 }
