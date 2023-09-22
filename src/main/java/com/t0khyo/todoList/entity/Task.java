da63145831ab60629e25855d64dfa41d7461f1e5
t0khyo <abdullrhman.eltoukhy@gmail.com>
2023-09-22 12:13:12 +0300

    Adding lombok to the Task entity

--- src/main/java/com/t0khyo/todoList/entity/Task.java
+++ src/main/java/com/t0khyo/todoList/entity/Task.java
@@ -2,12 +2,15 @@ package com.t0khyo.todoList.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
 @Entity
 @Table(name = "task")
+@Getter
+@Setter
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,6 +20,7 @@ public class Task {
     @Column(name = "title", nullable = false)
     private String title;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "status", nullable = false)
     private TaskStatus status;
 
@@ -40,54 +44,7 @@ public class Task {
         this.dueDate = dueDate;
     }
 
-    // setters/getters
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        if (title == null || title.trim().isEmpty()) {
-            throw new IllegalArgumentException("title cannot be empty");
-        }
-        this.title = title;
-    }
-
-    public TaskStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(TaskStatus status) {
-        this.status = Objects.requireNonNullElse(status, TaskStatus.INCOMPLETE);
-    }
-
-
-    public LocalDate getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueDate(LocalDate dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public TodoList getTodoList() {
-        return todoList;
-    }
-
-    public void setTodoList(TodoList todoList) {
-        this.todoList = todoList;
-    }
-
     // toString
-
     @Override
     public String toString() {
         return "Task{" + "id=" + id + ", title='" + title + '\'' + ", status=" + status + ", dueDate=" + dueDate + ", todoList=" + todoList.getName() + '}';
